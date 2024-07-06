4c185832623606f6d03127a0a8f5ee0b4c5be455
dev-ty <ty.chan383@gmail.com>
2024-07-06 14:56:46 +0700

    Chan Ty Landing Page

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="landing-page">
+      <div class="overlap">
+        <div class="rectangle"></div>
+        <div class="banner-heading">
+          <div class="element">
+            <div class="text-wrapper">Great design is invisible</div>
+            <p class="div">Lorem ipsum dolar sit amet constreteur</p>
+          </div>
+          <button class="button-component"><div class="text-wrapper-2">Button</div></button>
+        </div>
+        <div class="rectangle-2"></div>
+        <img class="vector" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" />
+      </div>
+      <header class="header">
+        <button class="div-wrapper"><div class="text-wrapper-3">Button</div></button>
+        <div class="logo-large">
+
+        </div>
+        <div class="nav-link-grp">
+          <div class="nav-link-comp"><div class="text-wrapper-4">Link 1</div></div>
+          <div class="nav-link-wrapper"><div class="text-wrapper-4">Link 2</div></div>
+          <div class="nav-link-wrapper"><div class="text-wrapper-4">Link 3</div></div>
+          <div class="nav-link-wrapper"><div class="text-wrapper-4">Link 4</div></div>
+          <div class="text-wrapper-4">Link 5</div>
+        </div>
+      </header>
+      <div class="overlap-group">
+        <div class="text-wrapper-5">Our Partners</div>
+        <div class="logos-grp">
+          <img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyiHR51obalVq8EbqnD_-fjlOVl41vmAHdUA&s" />
+          <img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyiHR51obalVq8EbqnD_-fjlOVl41vmAHdUA&s" />
+          <img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyiHR51obalVq8EbqnD_-fjlOVl41vmAHdUA&s" />
+          <img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyiHR51obalVq8EbqnD_-fjlOVl41vmAHdUA&s" />
+        </div>
+        <div class="group">
+          <div class="text-wrapper-6">Our happy clients</div>
+          <div class="reviews-card">
+            <div class="overlap-group-2">
+              <p class="p">
+                Lorem ipsum dolor sit amet consectetur. Condimentum eget vitae ligula sed urna sit sagittis interdum a.
+                Blandit mattis mattis lobortis orci. Facilisis dui sagittis tempor egestas pellentesque eu maecenas.
+                Risus lectus nisl.
+              </p>
+              <img class="ellipse" src="https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg" />
+              <div class="review-details">
+                <div class="text-wrapper-7">Jacqueline Wright</div>
+                <div class="text-wrapper-8">Customer</div>
+              </div>
+            </div>
+          </div>
+          <div class="overlap-wrapper">
+            <div class="overlap-group-2">
+              <p class="p">
+                Lorem ipsum dolor sit amet consectetur. Condimentum eget vitae ligula sed urna sit sagittis interdum a.
+                Blandit mattis mattis lobortis orci. Facilisis dui sagittis tempor egestas pellentesque eu maecenas.
+                Risus lectus nisl.
+              </p>
+              <img class="ellipse" src="https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg" />
+              <div class="review-details">
+                <div class="text-wrapper-7">Jacqueline Wright</div>
+                <div class="text-wrapper-8">Customer</div>
+              </div>
+            </div>
+          </div>
+          <div class="right-circle-icon">
+            <div class="ellipse-2"></div>
+            <img class="element-arrow-up" src="" />
+          </div>
+          <div class="right-circle-icon-2">
+            <div class="ellipse-3"></div>
+            <img class="img" src="" />
+          </div>
+        </div>
+        <div class="frame">
+          <div class="heading-grp">
+            <p class="text-wrapper-9">Our business strategy has helped many business</p>
+            <p class="text-wrapper-10">
+              Lorem ipsum dolor sit amet consectetur. Elementum nisl duis tortor sed. Suspendisse lobortis vitae quis
+              vehicula pellentesque sit id
+            </p>
+          </div>
+          <div class="overlap-2">
+            <div class="frame-2"></div>
+            <img class="img-lines-grp-i" src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-spring-summer-day-in-green-nature-mountains-free-photo.jpg?w=600&quality=80" />
+          </div>
+        </div>
+      </div>
+      <div class="frame-3">
+        <div class="text-wrapper-11">Latest Blog</div>
+        <div class="group-2">
+          <div class="resources-card">
+            <div class="overlap-group-3">
+              <div class="overlap-group-4">
+                <div class="div-2"></div>
+                <img class="img-2" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" />
+              </div>
+              <p class="text-wrapper-12">Humans are much more smarter than AI</p>
+              <p class="text-wrapper-13">
+                Lorem ipsum dolar sit amet consectetur. Lorem ipsum dolar sit amet consectetur.
+              </p>
+              <div class="text-wrapper-14">Learn more</div>
+            </div>
+          </div>
+          <div class="overlap-group-wrapper">
+            <div class="overlap-group-3">
+              <div class="overlap-group-4">
+                <div class="div-2"></div>
+                <img class="img-2" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" />
+              </div>
+              <p class="text-wrapper-12">Humans are much more smarter than AI</p>
+              <p class="text-wrapper-13">
+                Lorem ipsum dolar sit amet consectetur. Lorem ipsum dolar sit amet consectetur.
+              </p>
+              <div class="text-wrapper-14">Learn more</div>
+            </div>
+          </div>
+          <div class="resources-card-2">
+            <div class="overlap-group-3">
+              <div class="overlap-group-4">
+                <div class="div-2"></div>
+                <img class="img-2" src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" />
+              </div>
+              <p class="text-wrapper-12">Humans are much more smarter than AI</p>
+              <p class="text-wrapper-13">
+                Lorem ipsum dolar sit amet consectetur. Lorem ipsum dolar sit amet consectetur.
+              </p>
+              <div class="text-wrapper-14">Learn more</div>
+            </div>
+          </div>
+          <button class="button"><div class="text-wrapper-2">Button</div></button>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
